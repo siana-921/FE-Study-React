bb54aec180437dcbd31584a1794b05cb82eb0f52
수아 정 <mintolb12@gmail.com>
2023-01-16 16:22:53 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,76 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>my first react project 23-01-13</p>
-      </header>
-    </div>
-  );
+/*
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
+*/
+
+const customers = [
+  {
+    id: 1,
+    name: "sua",
+    image: "https://placeimg.com/64/64/1",
+    birthday: "920112",
+    gender: "여자",
+    job: "학생",
+  },
+  {
+    id: 2,
+    name: "sua2",
+    image: "https://placeimg.com/64/64/2",
+    birthday: "920113",
+    gender: "남자",
+    job: "학생",
+  },
+];
+
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+          <TableBody>
+            {customers.map((c) => {
+              return (
+                <Customer
+                  id={c.id}
+                  name={c.name}
+                  image={c.image}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                ></Customer>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 export default App;
